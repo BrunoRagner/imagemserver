93bc82439d4957a238ea819daedbbd913d13d856
brunoragner <brunooragnner@gmail.com>
2023-05-04 12:40:53 -0300

    primeiro  commit

---
+++ main.py
@@ -0,0 +1,75 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse, StreamingResponse
+from fastapi.responses import FileResponse
+from deta import Deta
+import json
+
+
+
+
+
+
+app = FastAPI()
+deta = Deta("b0aa59j8_QhoGru9anmve36LGgnuCBam5A1MJAVpC")  # configure your Deta project 
+drive = deta.Drive("img") # access to your drive
+
+
+
+
+
+@app.get("/lista")
+def list():
+    lis = drive.list()
+    return lis
+
+
+#formu
+@app.get("/", response_class=HTMLResponse)
+def render():
+    return FileResponse('files/index.html')
+
+
+#endpont retorno de formulario
+@app.post("/upload")
+def upload_img(file: UploadFile = File(...)):
+    name = file.filename
+    f = file.file
+    res = drive.put(name, f)
+    return res
+
+#endpont retorno de objeto
+@app.get("/download/{name}")
+def buscar_img(name: str):
+    res = drive.get(name)
+    return StreamingResponse(res.iter_chunks(1024), media_type="image/png")
+
+
+
+# endpont excluir de objeto
+@app.delete("/excluir/{name}")
+def delete_img(name: str):
+    ress = drive.delete(name)
+    return ress
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
